810371409ac660f2f5d826f6b29f7b1a170baec4
SayYoungMan <nickte89@gmail.com>
2026-03-26 11:22:06 +0900

    add nvim-tree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -13,4 +13,26 @@ return {
     -- use opts = {} for passing setup options
     -- this is equivalent to setup({}) function
   },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      'nvim-tree/nvim-web-devicons', -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    keys = {
+      { '<leader>tt', '<cmd>Neotree toggle<CR>', desc = '[T]oggle [T]ree' },
+    },
+    opts = {
+      window = {
+        mappings = {
+          ['l'] = 'open',
+          ['h'] = 'close_node',
+          ['<space>'] = 'none',
+        },
+      },
+    },
+  },
 }
