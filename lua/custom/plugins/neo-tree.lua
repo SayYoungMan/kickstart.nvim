8fd853c9bdf6f11f7c654ab5c638f1e4f0b6d5c3
SayYoungMan <nickte89@gmail.com>
2026-03-26 11:32:12 +0900

    custom plugins

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,18 +1,4 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-
----@module 'lazy'
----@type LazySpec
 return {
-  {
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    config = true,
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
-  },
   {
     'nvim-neo-tree/neo-tree.nvim',
     branch = 'v3.x',
